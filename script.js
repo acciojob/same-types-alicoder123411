147ce22ae55615bf2a86118285df5489d7195264
alicoder123411 <sameersingla240@gmail.com>
2026-01-14 22:28:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function isSameType(value1, value2) {
-    if (Number.isNaN(value1) && Number.isNaN(value2)) {
-        return true;
+    // If either value is NaN, return false
+    if (Number.isNaN(value1) || Number.isNaN(value2)) {
+        return false;
     }
+
     return typeof value1 === typeof value2;
 }
 
